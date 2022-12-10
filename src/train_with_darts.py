cef7178c097313aa481bb9a4347b6d41baddb154
fantauzzi <Francesco_Fantauzzi@yahoo.com>
2022-12-10 19:29:29 +0100

    Computing and logging metrics from backtesting

--- src/train_with_darts.py
+++ src/train_with_darts.py
@@ -1,18 +1,18 @@
 import os
 from logging import info
-from pathlib import Path
-# from autogluon.timeseries import TimeSeriesDataFrame, TimeSeriesPredictor
-# from autogluon.timeseries.splitter import MultiWindowSplitter
-from darts import TimeSeries, metrics
-from darts.models import NaiveSeasonal
+
+import pandas as pd
+from darts import TimeSeries
+from darts.models import ARIMA
 from omegaconf import DictConfig
 import hydra
 import mlflow as mf
-from utils import boostrap_pipeline_component, get_data_filename, load_daily_price_adjusted, \
-    get_autogluon_dir, log_outputs, log_dataframe
+from utils import boostrap_pipeline_component, get_data_filename, load_daily_price_adjusted, log_outputs
+import numpy as np
 
 
-# mlflow run src -e train --experiment-name default_experiment
+# mlflow run src -e train_with_darts --experiment-name default_experiment
+
 
 @hydra.main(config_path='../config', config_name='params')
 def main(params: DictConfig) -> None:
@@ -35,79 +35,57 @@ def main(params: DictConfig) -> None:
         ts_train_val = TimeSeries.from_dataframe(df=df_train_val, value_cols='adjusted_close')
 
         prediction_length = params.training.prediction_length
-        model = NaiveSeasonal()
-        res = model.backtest(series=ts_train_val,
-                             start=prediction_length * 52,
-                             forecast_horizon=prediction_length,
-                             stride=1,
-                             retrain=True,
-                             overlap_end=False,
-                             last_points_only=False,
-                             metric=metrics.mse,
-                             reduction=None,
-                             verbose=True)
-        print(res)
-
-        """
+        model = ARIMA()
+        saved_model = f'../{params.main.saved_model}'
         # noinspection PyArgumentList
-        res = model.historical_forecasts(series+=ts_train_val,
-                                         start=5 * 52,
-                                         forecast_horizon=5,
-                                         stride=5,
-                                         retrain=True,
-                                         overlap_end=False,
-                                         last_points_only=False,
-                                         verbose=True)
-                                         """
+        forecasts = model.historical_forecasts(series=ts_train_val,
+                                               start=params.training.start_training_at,
+                                               forecast_horizon=prediction_length,
+                                               stride=params.training.stride,
+                                               retrain=True,
+                                               overlap_end=False,
+                                               last_points_only=False,
+                                               verbose=True)
 
-        # print(res)
+        """ Compute the difference (with sign) forecast-actual and stores it in a DataFrame; every line corresponds
+        to the set of forecasts made at a given time (in the line index) for the forecasting horizon (one forecast
+        per column) """
 
-        """     
-        num_windows = params.training.num_windows
-        test_data = ts_df
-        train_data = ts_df.slice_by_timestep(None, -prediction_length * num_windows)
-        autogluon_dir = get_autogluon_dir(params)
+        differences_dict = {i: [] for i in range(prediction_length)}
+        forecasts_timestamp = []
+        for metrics_series in forecasts:
+            metrics_series_pd = metrics_series.pd_series()
+            time_at_forecast = metrics_series_pd.index[0] - 1
+            forecasts_timestamp.append(time_at_forecast)
+            for i in range(time_at_forecast, time_at_forecast + prediction_length):
+                diff = (metrics_series_pd[i + 1] - df_train_val.adjusted_close[i + 1])
+                differences_dict[i - time_at_forecast].append(diff)
 
-        splitter = MultiWindowSplitter(num_windows=num_windows)
-        predictor = TimeSeriesPredictor(path=autogluon_dir,
-                                        target="adjusted_close",
-                                        prediction_length=prediction_length,
-                                        eval_metric=params.training.eval_metric,
-                                        ignore_time_index=True,
-                                        validation_splitter=splitter
-                                        )
+        differences_df = pd.DataFrame(differences_dict, index=forecasts_timestamp)
 
-        predictor.fit(train_data,
-                      presets=params.training.presets,
-                      time_limit=params.training.time_limit,
-                      )
+        model.save(saved_model)
+        info(f'Logging artifact {saved_model} in current run')
+        mf.log_artifact(saved_model)
 
-        mf.log_artifact(autogluon_dir)
-        artifact_uri = mf.get_artifact_uri()
-        info(f'Logged artifact {autogluon_dir} in current run with URI {artifact_uri}/{Path(autogluon_dir).name}')
+        info('Logging validation metrics in current run')
+        for idx, row in differences_df.iterrows():
+            # noinspection PyTypeChecker
+            se_to_log = {f'SE at T{item_idx + 1}': value ** 2 for item_idx, value in row.items()}
+            # noinspection PyTypeChecker
+            ae_to_log = {f'AE at T{item_idx + 1}': np.abs(value) for item_idx, value in row.items()}
+            # noinspection PyTypeChecker
+            mf.log_metrics(se_to_log, step=idx)
+            # noinspection PyTypeChecker
+            mf.log_metrics(ae_to_log, step=idx)
 
-        # score = predictor.evaluate(test_data)
-        # summary = predictor.fit_summary()
-
-        leaderboard = predictor.leaderboard(silent=False)
-        # predictor.leaderboard(data=train_data, silent=False)
-        leaderboard.set_index('model', inplace=True)
-        info('Logging the leaderboard to the current run')
-        log_dataframe(leaderboard)
-        if params.training.selected_model is None:
-            selected_model = predictor.get_model_best()
-            info(f'Selected model based on cross-validations: {selected_model}')
-        else:
-            selected_model = params.training.selected_model
-            info(f'Selected model based configuration parameters: {selected_model}')
-            best_model = predictor.get_model_best()
-            if selected_model != best_model:
-                info(f'Note: selected model is not the best based on cross-validation, which would be {best_model}')
-
-        # Now chose the model of choice with the best evaluation score, and re-fit it over the whole train-val dataset
+        mse_metrics = (differences_df ** 2).mean()
+        mae_metrics = np.abs(differences_df).mean()
+        mse_to_log = {f'MSE at T{item_idx + 1}': value for item_idx, value in mse_metrics.items()}
+        mae_to_log = {f'MAE at T{item_idx + 1}': value for item_idx, value in mae_metrics.items()}
+        mf.log_metrics(mse_to_log)
+        mf.log_metrics(mae_to_log)
 
 
-        """
     except Exception as ex:
         raise ex
     finally:
@@ -117,8 +95,12 @@ def main(params: DictConfig) -> None:
 if __name__ == '__main__':
     main()
 
-""" TODO Open questions
-When using multi-window backtesting, is the validation metric the average of the validation metrics?
-When using multi-window backtesting, is the model re-trained on all the available data?
-
+""" TODO
+Add the MAPE metric (% error) 
+Automate experiments on a list of symbols. Should those be uni or multi variate?
+Throw in an index as an exogenous variable
+from source_data, save two different csv files for train/val and test
+Implement the testing, draw charts of predictions vs. actual
+Try different models, do hyperparameters tuning (Optuna?)
+Explain the model
 """
